d9ab207a47367d99377664255b6899c4d6b96eb4
WizaXxX <wizax0@gmail.com>
2022-05-25 21:27:56 +0300

    add recognazer and scrollview

--- lessons/2. Пользовательский интерфейс iOS-приложений/vk/vk/LoginViewController.swift
+++ lessons/2. Пользовательский интерфейс iOS-приложений/vk/vk/LoginViewController.swift
@@ -11,13 +11,62 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let hideKeyboardGesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(hideKeyboard))
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
     }
 
     @IBAction func Enter(_ sender: Any) {
+        
+        guard loginField.text != nil else {
+            showAlert(title: "Ошибка входа", message: "Укажите логин", actions: [getRetryAction()])
+            return
+        }
+        
+        guard passwordField.text != nil else {
+            showAlert(title: "Ошибка входа", message: "Укажите пароль", actions: [getRetryAction()])
+            return
+        }
+        
         let login = loginField.text!
         let password = passwordField.text!
         
@@ -25,20 +74,33 @@ class LoginViewController: UIViewController {
             showAlert(
                 title: "Ошибка входа",
                 message: "Для входа необходимо заполнить логин и пароль!",
-                actions: [UIAlertAction(title: "Повторить попытку", style: UIAlertAction.Style.default, handler: nil)])
+                actions: [getRetryAction()])
             return
         }
         
-        showAlert(
-            title: "Вход в систему",
-            message: "Вы пытаетесь войти под пользователем \(login) c паролем \(password)",
-            actions: [
-                UIAlertAction(title: "Продолжить вход", style: UIAlertAction.Style.default, handler: { _ in print("Вход выполнен") }),
-                UIAlertAction(title: "Отменить", style: UIAlertAction.Style.cancel, handler: { _ in print("Вход отменен")})],
-            needActions: true)
+        if login == "admin" && password == "123" {
+            showAlert(
+                title: "Вход в приложение",
+                message: "Вы вошли в систему",
+                actions: [UIAlertAction(title: "Продолжить вход", style: UIAlertAction.Style.default, handler: { _ in print("Вход выполнен") })])
+        } else {
+            showAlert(
+                title: "Вход в приложение",
+                message: "Не верный логин или пароль",
+                actions: [
+                    getRetryAction(),
+                    UIAlertAction(
+                        title: "Закрыть приложение",
+                        style: UIAlertAction.Style.cancel,
+                        handler: { _ in exit(0)})])
+        }
     }
     
-    func showAlert(title: String, message: String, actions: [UIAlertAction], needActions: Bool = false) {
+    func getRetryAction() -> UIAlertAction {
+        return UIAlertAction(title: "Повторить попытку", style: UIAlertAction.Style.default, handler: nil)
+    }
+    
+    func showAlert(title: String, message: String, actions: [UIAlertAction]) {
         let alert = UIAlertController(
             title: title,
             message: message,
@@ -49,6 +111,31 @@ class LoginViewController: UIViewController {
         }
         
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        
+        let buttonFrame: CGRect = enterButton.frame
+        let info = notification.userInfo! as NSDictionary
+        let keyboardFrame = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue
+        
+        if buttonFrame.origin.y <= keyboardFrame.origin.y {
+            return
+        }
+        
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.size.height, right: 0.0)
+   
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        scrollView?.contentInset = UIEdgeInsets.zero
+    }
+    
+    @objc func hideKeyboard() {
+        scrollView?.endEditing(true)
     }
 }
 
